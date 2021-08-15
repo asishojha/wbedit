df04ad46857ac52f506d78e8a9654d7929f474b4
singlagarvit <singlagarvit68@gmail.com>
2021-08-15 16:32:56 +0530

    Demo Complete Changes:

--- student/templatetags/custom_filters.py
+++ student/templatetags/custom_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter(name='date_format')
 def date_format(value):
 	return datetime.strptime(value, '%d%m%y')
+
+@register.filter(name='index_format')
+def index_format(value):
+	return '-'.join([value[:2], value[2:]])
