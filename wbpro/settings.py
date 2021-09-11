edf83a4044d4bfa2cdbee79686608c68ad978f2f
singlagarvit <singlagarvit68@gmail.com>
2021-09-11 20:05:17 +0530

    DB settings

--- wbpro/settings.py
+++ wbpro/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'wbpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'WBEDIT',
+        'USER': 'asish',
+        'PASSWORD': 'netcom12',
+        'HOST': '20.198.101.155',
+        'PORT': '5432',
     }
 }
 
