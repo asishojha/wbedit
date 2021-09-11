e96833648a2b1cbd2ba6c0e03e3b18c85100f56b
singlagarvit <singlagarvit68@gmail.com>
2021-09-11 20:09:14 +0530

    DB settings

--- wbpro/settings.py
+++ wbpro/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'wbpro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'WBEDIT',
+        'NAME': 'wbedit',
         'USER': 'asish',
         'PASSWORD': 'netcom12',
         'HOST': '20.198.101.155',
