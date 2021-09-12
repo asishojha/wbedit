3cb1a409b87220c0c422b28a6adf60bb8c2f8249
singlagarvit <singlagarvit68@gmail.com>
2021-09-12 20:53:31 +0530

    Database Conf

--- wbpro/settings.py
+++ wbpro/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'wbpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'wbedit',
-#         'USER': 'asish',
-#         'PASSWORD': 'netcom12',
-#         'HOST': '20.198.101.155',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wbedit',
+        'USER': 'asish',
+        'PASSWORD': 'netcom12',
+        'HOST': '20.198.101.155',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
