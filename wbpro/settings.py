ac2d19c58c8fd0283e25c6efc3e5f86f49a9abac
singlagarvit <singlagarvit68@gmail.com>
2021-09-13 10:06:54 +0530

    Final Host

--- wbpro/settings.py
+++ wbpro/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['wbbsedata.com']
+ALLOWED_HOSTS = ['www.wbbsedata.com']
 
 
 # Application definition
