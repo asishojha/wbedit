bb7cf012c95c041da12cc59c62f27eb4b2b9f33f
singlagarvit <singlagarvit68@gmail.com>
2021-09-27 19:34:53 +0530

    Download PDFs

--- school/downloads.py
+++ school/downloads.py
@@ -2,21 +2,9 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from weasyprint import HTML
-import tempfile
 
 def download_pdf(school):
 	html_string = render_to_string('school/pdf-report.html', {'school': school})
 	html = HTML(string=html_string)
-	result = html.write_pdf()
-
-	response = HttpResponse(content_type='application/pdf')
-	response['Content-Disposition'] = 'inline; filename="{}.pdf"'.format(school.username)
-	response['Content-Transfer-Encoding'] = 'binary'
-
-	with tempfile.NamedTemporaryFile(delete=True) as output:
-		output.write(result)
-		output.flush()
-		output = open(output.name, 'r')
-		response = write(output.read())
-
-	return response
+	html.write_pdf("pdfs/{}.pdf".format(school.username));
+	return None
